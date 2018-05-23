a018498df03c4bf49cada3677ff554206872756f
steff456 <steff456@hotmail.com>
2018-05-23 02:02:28 -0500

    se generan nuevas imagenes

--- AugmData/__main__.py
+++ AugmData/__main__.py
@@ -2,11 +2,10 @@
 # !/usr/bin/env python3
 
 """Main for the generation of new inkml"""
+import argparse
 import logging
 import coloredlogs
 
-from AugmData.load import load_txt
-from AugmData.new_inkml import new_inkml
 from AugmData.new_inkml import generate
 
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
@@ -18,16 +17,20 @@ parser = argparse.ArgumentParser(
     description='Increase number of images')
 
 parser.add_argument('--directory', type=str, default='./trainingSymbols',
-                    help='directory of the inkml files')
-parser.add_argument('--index-txt', type=str, default='training_index.txt',
+                    help='directory of the original inkml files')
+parser.add_argument('--index-txt', type=str, default='./training_index.txt',
                     help='file with annotations')
+parser.add_argument('--num-im', type=int, default=5,
+                    help='number of new images')
+parser.add_argument('--out-dir', type=str, default='./generatedSymbols',
+                    help='directory of the generated inkml files')
 
+args = parser.parse_args()
 
 def load_txt(file, directory):
     '''Loads the routes of the existing inkml files'''
     with open(file) as f:
         a = f.readlines()
-
     d = {}
     for line in a:
         line = line.strip()
@@ -35,13 +38,19 @@ def load_txt(file, directory):
         data = parts[0].split('_')
         name = data[-1]
         if parts[1] in d:
-            d[parts[1]].append(directory + 'iso' + name + '.inkml')
+            d[parts[1]].append(directory + '/' + 'iso' + name + '.inkml')
         else:
-            d[parts[1]] = [directory + 'iso' + name + '.inkml']
+            d[parts[1]] = [directory + '/' + 'iso' + name + '.inkml']
 
     return d
 
 
 def main():
+    print('*** Loading original txt ***')
     original = load_txt(args.index_txt, args.directory)
-    generate(original)
+    print('*** Generating ***')
+    generate(original, args.num_im, args.out_dir)
+
+
+if __name__ == '__main__':
+    main()
--- AugmData/new_inkml.py
+++ AugmData/new_inkml.py
@@ -1,12 +1,15 @@
+# -*- coding: utf-8 -*-
+import math
+import random
 import xml.etree.ElementTree as ET
 # import matplotlib.pyplot as plt
 # import numpy as np
 
 
-def new_inkml(route='./trainingSymbols/iso4.inkml'):
+def new_coordinates(route):
+    ''' Calculate new coordinates from inkml of the route '''
     NS = {'ns': 'http://www.w3.org/2003/InkML',
           'xml': 'http://www.w3.org/XML/1998/namespace'}
-
     # charge image
     tree = ET.parse(route)
     root = tree.getroot()
@@ -17,35 +20,86 @@ def new_inkml(route='./trainingSymbols/iso4.inkml'):
     b = root.findall('ns:trace', namespaces=NS)
     strokes = []
     coord_strokes = {}
+    # check all strokes
     for i in range(0, len(b)):
         strokes.append(b[i].text.strip())
         coord = strokes[-1].split(',')
-        xcoord = []
-        ycoord = []
+        new_coord = ''
+        first = True
+        # generate new pairs
         for pair in coord:
             pair = pair.strip()
             pair = pair.split(' ')
-            x = pair[0]
-            y = pair[1]
-            xcoord.append(int(x))
-            ycoord.append(int(y))
-            print(x)
-            print(y)
+            x = float(pair[0])
+            y = float(pair[1])
+            rand_x = random.random()/2.0
+            # rand_x = 0
+            nx = x + rand_x
+            rand_y = random.random()/2.0
+            # rand_y = 0
+            ny = y + rand_y
+            print(str(x) + ';' + str(nx))
+            print(str(y) + ';' + str(ny))
             print('---')
-        coord_strokes[i] = [xcoord, ycoord]
+            strx = str(nx)
+            stry = str(ny)
+            if first:
+                new_coord = strx + ' ' + stry
+                first = False
+            else:
+                new_coord = new_coord + ', ' + strx + ' ' + stry
+        coord_strokes[i] = new_coord
 
-    # plt.imshow(f_arr, cmap=plt.get_cmap('gray'))
-    # plt.show()
+    return coord_strokes
 
 
-def generate(or_dict, num_im=7940):
+def writeInkML(org_inkml, n_coord, out_dir, count):
+    '''Write the generated inkml from the original inkml'''
+    name = out_dir + '/iso' + str(count) + '.inkml'
+    outputfile = open(name, 'w')
+
+    with open(org_inkml) as f:
+        a = f.readlines()
+
+    change = False
+    count = 0
+    for line in a:
+        if line.startswith('<trace id='):
+            change = True
+            # print(line)
+            outputfile.write(line)
+            outputfile.write(n_coord[count] + '\n')
+            # print(n_coord[count])
+            count += 1
+        elif change:
+            change = False
+        else:
+            # print(line)
+            outputfile.write(line)
+
+    outputfile.close()
+    return name
+
+
+def writeLabels(route, label, name, count):
+    '''Write in a txt the label of the generated inkml'''
+    with open('./test.txt', "a") as f:
+        f.write(name + "_" + str(count) + ", " + label + '\n')
+
+
+def generate(or_dict, num_im, out_directory):
     '''Function that generates the desired number of images per class'''
     n_dict = or_dict
-    for key in n_dict.keys():
-        count = 0
+    count = 0
+    for key in or_dict.keys():
+        in_count = 0
         while(len(n_dict[key]) < num_im):
-            i = count % len(or_dict[key])
+            i = in_count % len(or_dict[key])
             inkml_act = or_dict[key][i]
-            n = new_inkml(inkml_act)
-            n_dict[key].append(n)
+            print(inkml_act)
+            n_coord = new_coordinates(inkml_act)
+            name = writeInkML(inkml_act, n_coord, out_directory, count)
+            writeLabels(out_directory, key, name, count)
+            n_dict[key].append(name)
+            in_count += 1
             count += 1
