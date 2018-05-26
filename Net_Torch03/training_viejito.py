87ff2df96ff0e47e455358ec7d94e6820d4d3e8a
steff456 <steff456@hotmail.com>
2018-05-25 23:10:25 -0500

    mas

--- Net_Torch03/training_viejito.py
+++ Net_Torch03/training_viejito.py
@@ -106,8 +106,8 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 transform = transforms.Compose([
-    ToTensor(),
-    Normalize(
+    transforms.ToTensor(),
+    transforms.Normalize(
         mean=[0.485, 0.456, 0.406],
         std=[0.229, 0.224, 0.225])
 ])
