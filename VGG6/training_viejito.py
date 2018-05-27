d9c38c38bc510af04a5508bcae87f71eb52afd75
steff456 <steff456@hotmail.com>
2018-05-26 20:01:26 -0500

    jsdkla

--- VGG6/training_viejito.py
+++ VGG6/training_viejito.py
@@ -153,7 +153,7 @@ class Net(nn.Module):
         # self.conv8 = nn.Conv2d(256, 512, 3)
         # self.conv9 = nn.Conv2d(512, 512, 3)
         # self.conv10 = nn.Conv2d(512, 512, 3)
-        self.fc1 = nn.Linear(12544, 1024)
+        self.fc1 = nn.Linear(6400, 1024)
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, 102)
 
@@ -173,7 +173,7 @@ class Net(nn.Module):
         # x = F.selu(self.conv10(x))
         # print(x.size())
         x = x.view(x.size(0), -1)
-        print(x.size())
+        # print(x.size())
         x = self.fc1(x)
         x = self.fc2(x)
         x = self.fc3(x)
